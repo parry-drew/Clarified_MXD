b295dde97742017d6e06e52f0f70527679201d5d
Drew <pdrew2@dot.nycnet>
2017-10-31 13:21:55 -0400

    First Push

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Clarified_MXD
+# Clarified MXD
+
+This script will create and populate a file gdb with all the unique data sources used in an mxd. Ideal for when you need to move an mxd with a lot of sde sources to a closed system.
+
+### Directions
+1. Open the command line terminal.
+
+2. Change Directory and run the python script
+```
+cd project_directory
+C:\Python27\ArcGIS10.4\python.exe clarified_mxd.py
+```
+3. You will be prompted to provide the full directory for your mxd. Just drag and drop the file into the command line window and click enter.
+
+4. You will also be prompted to provide a name for the gdb where all the sources are being outputed to.
+
+5. After the scripted has run you will have the file gdb and a zipped version.
---
+++ clarified_mxd.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# ---------------------------------------------------------------------------
+# mxd_clarify.py
+# This script will create and populate a file gdb with all the unique data sources
+# used in an mxd.
+#
+# C:\Python27\ArcGIS10.4\python.exe clarified_mxd.py
+#
+# ---------------------------------------------------------------------------
+import os, datetime, timeit, shutil, zipfile, arcpy
+
+root = os.path.abspath(os.path.curdir)
+
+def main():
+    #raw_mxd = raw_input('    1. What is the full directory of the mxd you want to clarify? --> ')
+    raw_gdb = raw_input('    2. What do you want to to call the gdb? --> ')
+    start = timeit.default_timer()
+    #print("\n    PROCESSING")
+    #arcpy.CreateFileGDB_management(root, raw_gdb)
+    #mxd = arcpy.mapping.MapDocument(raw_mxd)
+    #raw_sources = []
+    # Creates a list of data sources
+    #for lyr in arcpy.mapping.ListLayers(mxd):
+        #if lyr.supports("DATASOURCE"):
+            #raw_sources.append(lyr.dataSource)
+    # Remove duplicate list values
+    #sources = list(set(raw_sources))
+    # Populates the gdb.
+    #arcpy.FeatureClassToGeodatabase_conversion(sources , root + "\\" + raw_gdb + ".gdb")
+    # Zip gdb
+    #ZipFile( raw_gdb + '.zip', mode='w').write(raw_gdb + ".gdb")
+    outFile = raw_gdb + '.zip'
+    gdbName = os.path.basename(raw_gdb)
+    with zipfile.ZipFile(outFile,mode='w',compression=zipfile.ZIP_DEFLATED,allowZip64=True) as myzip:
+        for f in os.listdir(raw_gdb + ".gdb"):
+            myzip.write(os.path.join(raw_gdb + ".gdb",f),gdbName+'\\'+os.path.basename(f))
+
+    stop = timeit.default_timer()
+    print("\n    COMPLETED! Total Run Time: " +  str(stop - start) + " seconds")
+
+if __name__ == '__main__':
+    main()
